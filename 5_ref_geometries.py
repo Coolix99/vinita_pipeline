0b901ced347c545a29d0836e0f7bc9aff203d404
Max <kotzmax@gmail.com>
2025-02-20 21:06:57 +0100

    Enhance final plots and reference geometries: integrate temporal histogram interpolation and movie generation; clean up unused code and improve data handling for Smoc intensity values.

--- 5_ref_geometries.py
+++ 5_ref_geometries.py
@@ -1,6 +1,6 @@
 from zf_pf_diffeo.pipeline import do_referenceGeometries,do_temporalreferenceGeometries
-from zf_pf_diffeo.plot_movie import show_temporal_mesh_evolution
-import os
+
+
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -14,6 +14,3 @@ if __name__ == "__main__":
     do_referenceGeometries(proj_dir,["experimentalist","genotype","condition","time in hpf"],maps_dir,"projected_data","Projected Surface file name")
     do_temporalreferenceGeometries(maps_dir, "time in hpf", ["experimentalist","genotype","condition"], temp_maps_dir)
 
-    
-    
-    show_temporal_mesh_evolution(os.path.join(temp_maps_dir,"Vinita_WT_Development"))
--- 7_final_plots.py
+++ 7_final_plots.py
@@ -1,9 +1,25 @@
-from zf_pf_diffeo.plot_static import plot_all_reference_meshes,plot_all_reference_data
+import os
 import numpy as np
+from zf_pf_diffeo.plot_movie import show_temporal_mesh_evolution,movie_temporal_hist_evolution
+from zf_pf_diffeo.plot_static import plot_all_reference_meshes,plot_all_reference_data
+from zf_pf_diffeo.pipeline import do_temporalHistInterpolation
+
 def data_to_value_function(hist_data):
-        print("Keys in .npz file:", hist_data.files)
+        #print("Keys in .npz file:", hist_data.files)
         
         totalcc = np.array([np.median(cc) for cc in hist_data['BRE_max_intensity']])
+        
+        mean_value = np.nanmean(totalcc)
+
+        # Replace NaN values with the computed mean
+        totalcc[np.isnan(totalcc)] = mean_value
+        #print(totalcc)
+        return totalcc
+
+def data_to_value_function_Smoc(hist_data):
+        #print("Keys in .npz file:", hist_data.files)
+        
+        totalcc = np.array([np.median(cc) for cc in hist_data['Smoc_max_intensity']])
         
         mean_value = np.nanmean(totalcc)
 
@@ -17,5 +33,13 @@ if __name__ == "__main__":
     maps_dir = "/home/max/Documents/02_Data/structured_data/structured_vinita/Maps"
     proj_dir = "/home/max/Documents/02_Data/structured_data/structured_vinita/Projected"
     
-    plot_all_reference_meshes(maps_dir, scale_unit="µm")
-    plot_all_reference_data(maps_dir, data_to_value_function, scale_unit="µm")
+#     plot_all_reference_meshes(maps_dir, scale_unit="µm")
+#     plot_all_reference_data(maps_dir, data_to_value_function_Smoc, scale_unit="µm",separate_windows=True)
+
+    temp_maps_dir = "/home/max/Documents/02_Data/structured_data/structured_vinita/Maps_temp"    
+#     show_temporal_mesh_evolution(os.path.join(temp_maps_dir,"Vinita_WT_Development"))
+    
+    do_temporalHistInterpolation(proj_dir,temp_maps_dir, "time in hpf", ["experimentalist","genotype","condition"], {"BRE": data_to_value_function},surface_key="projected_data",surface_file_key="Projected Surface file name")
+
+    movie_temporal_hist_evolution(os.path.join(temp_maps_dir,"Vinita_WT_Development"),save_path="/home/max/Downloads/VINITA/dev_BRE.mp4",scale_unit="µm")
+    movie_temporal_hist_evolution(os.path.join(temp_maps_dir,"Vinita_WT_Regeneration"),save_path="/home/max/Downloads/VINITA/reg_BRE.mp4",scale_unit="µm")
